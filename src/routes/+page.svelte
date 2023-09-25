7b2087b1d950d28469a68f2ef0836f721755c5d9
DoChau <dntamchau@gmail.com>
2023-09-26 00:23:14 +0700

    feat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,1 @@
-<script>
-	import { onMount, afterUpdate } from 'svelte';
-	import { goto } from '$app/navigation';
-
-	//*let isAuthenticated = getItem('access_token');
-
-	onMount(() => {
-			goto(`/dashboard`)
-	});
-
-</script>
-
-<svelte:head>
-	<title>Login</title>
-</svelte:head>
 
